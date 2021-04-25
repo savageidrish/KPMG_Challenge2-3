5759639fa2449a052a89457bc706108bc0ea2027
savageidrish <46934406+savageidrish@users.noreply.github.com>
2021-04-25 14:11:03 +0530

    Update metadata-script.sh

--- Challenge2/metadata-script.sh
+++ Challenge2/metadata-script.sh
@@ -6,6 +6,6 @@ curl http://169.254.169.254/latest/dynamic/instance-identity/document
 
 
 
-#To retrieve a specific data for example - regoin of the EC2 instance
+#To retrieve a specific data for example - region of the EC2 instance
 
 curl http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region
